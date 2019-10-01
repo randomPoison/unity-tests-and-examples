1c24004839e7e65ccc7730cefbea7b044e9dbd31
David LeGare <dlegare.1001@gmail.com>
2019-10-01 14:43:43 -0500

    Add UniTask package

--- Assets/Scripts/AsyncTests/AsyncTestController.cs
+++ Assets/Scripts/AsyncTests/AsyncTestController.cs
@@ -1,18 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AsyncTestController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private async void Start()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
