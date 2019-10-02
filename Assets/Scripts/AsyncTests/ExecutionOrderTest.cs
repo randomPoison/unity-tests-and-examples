ae2982a2ba1c97bf3ce7323d3c2618cfabdbe692
David LeGare <dlegare.1001@gmail.com>
2019-10-02 17:13:41 -0500

    Split execution order test into separate script

---
+++ Assets/Scripts/AsyncTests/ExecutionOrderTest.cs
@@ -0,0 +1,20 @@
+﻿using UniRx.Async;
+using UnityEngine;
+
+public class ExecutionOrderTest : MonoBehaviour
+{
+
+    private void Start()
+    {
+        Debug.Log("About to call VoidTask()");
+        VoidTask().Forget();
+        Debug.Log("Returned from VoidTask()");
+    }
+
+    private async UniTaskVoid VoidTask()
+    {
+        Debug.Log("Doing a void task!");
+        await UniTask.Delay(1000);
+        Debug.Log("Void task resumed!");
+    }
+}
