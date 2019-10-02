3eef34b0c5d345a725001dea922160fc2879b8f0
David LeGare <dlegare.1001@gmail.com>
2019-10-02 15:26:29 -0500

    Add timing test

--- Assets/Scripts/AsyncTests/AsyncTestController.cs
+++ Assets/Scripts/AsyncTests/AsyncTestController.cs
@@ -9,6 +9,7 @@ public class AsyncTestController : MonoBehaviour
         VoidTask();
         Debug.Log("Returned from VoidTask()");
     }
+
     private async void VoidTask()
     {
         Debug.Log("Doing a void task!");
@@ -18,6 +19,9 @@ public class AsyncTestController : MonoBehaviour
 
     private async void Start()
     {
+        LongRunningTask();
+        TimingTest();
+
         Debug.Log("AsyncTestController.Start()");
 
         Debug.Log($"Destroying game object {this}");
@@ -33,5 +37,42 @@ public class AsyncTestController : MonoBehaviour
         await UniTask.Delay(10);
         Debug.Log("StringTask(), after await");
         return "Did a thing!";
+    }
+
+    private async void LongRunningTask()
+    {
+        var start = Time.realtimeSinceStartup;
+        while (Time.realtimeSinceStartup - start < 100f)
+        {
+            var randomNumber = await SlowRandom();
+            Debug.Log($"Random number was {randomNumber}");
+        }
+    }
+
+    private async UniTask<int> SlowRandom()
+    {
+        await UniTask.Delay(3000);
+        return Random.Range(0, 10);
+    }
+
+    private async void TimingTest()
+    {
+        Debug.Log($"Simulating loading an asset for the first time, frame: {Time.frameCount}");
+        var asset = await LoadAssetCached(false);
+        Debug.Log($"Got asset on frame {Time.frameCount}: {asset}");
+
+        Debug.Log($"Simulating load from cache, frame: {Time.frameCount}");
+        asset = await LoadAssetCached(true);
+        Debug.Log($"Got asset on frame {Time.frameCount}: {asset}");
+
+        async UniTask<string> LoadAssetCached(bool isCached)
+        {
+            if (!isCached)
+            {
+                await UniTask.Delay(1000);
+            }
+
+            return "I'm an asset!";
+        }
     }
 }
