d45b93f494fe3ad6eaa42bfe8cc46bfa680c5342
David LeGare <dlegare.1001@gmail.com>
2019-11-25 20:54:24 -0600

    Add some more bitcode plugin tests

--- Assets/Plugins/cpp_test.cpp
+++ Assets/Plugins/cpp_test.cpp
@@ -1,4 +1,10 @@
+#include <vector>
+
 extern "C" int add(int left, int right)
 {
-    return left + right;
+    std::vector<int> values;
+    values.push_back(left);
+    values.push_back(right);
+
+    return values[0] + values[1];
 }
